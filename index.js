131170d845eaedac9c2d87c687f34421d84ab7a6
Olga Simankova <o.kotlyarova@gmail.com>
2023-02-03 21:50:04 +0300

    feat: add modal

---
+++ index.js
@@ -0,0 +1,8 @@
+const buttonOneClickHandler = () => {
+  document.querySelector(".first_line_block_1").classList.toggle("hidden");
+};
+
+const buttonTwoClickHandler = () => {
+  document.querySelector(".second_line_block_1").classList.toggle("swap_1");
+  document.querySelector(".second_line_block_2").classList.toggle("swap_2");
+};
